d2b5841436ab7b6993caf2f509825df2f51fac6a
Kiran Kumar S K <57290570+kirankumarsk@users.noreply.github.com>
2021-03-17 07:33:03 +0530

    Add files via upload

---
+++ new intro/temperature.java
@@ -0,0 +1,15 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		double fahrenheit = 91;
+		
+		double celsius = (fahrenheit - 32) * 5.0/9.0;
+		
+		System.out.println(fahrenheit + " degrees Fahrenheit is " + celsius + " degrees celsius.");
+		
+		// TODO Make formatting nicer
+	}
+
+}
