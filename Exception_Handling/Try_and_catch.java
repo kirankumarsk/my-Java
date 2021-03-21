7f373059be563e52acfc9cc5c0cfd9e2dc09a89c
Kiran Kumar S K <57290570+kirankumarsk@users.noreply.github.com>
2021-03-21 14:26:30 +0530

    Add files via upload

---
+++ Exception_Handling/Try_and_catch.java
@@ -0,0 +1,29 @@
+package Exception_Handling;
+
+import java.util.*;
+
+class Question1 {
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        boolean continueInput = true;
+
+        // Prompt the user to enter two integers
+        System.out.print("Enter two numbers: ");
+        do {
+            try {
+                int number1 = input.nextInt();
+                int number2 = input.nextInt();
+
+                // Display the result
+                System.out.println("The sum is " + (number1 + number2));
+
+                continueInput = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("Try again. (" + "Incorrect input: two integers are required) ");
+                input.nextLine(); // Discard input
+            }
+        } while (continueInput);
+        input.close();
+    }
+}
